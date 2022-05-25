23179cb760fb45ab4f173d785ce5530576147375
Jose Junior <jose.junior@porlande.com>
2022-05-25 16:16:09 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular13-pdv',
+    appId: '1:608200548307:web:50ecfa99309ac2dc929926',
+    storageBucket: 'angular13-pdv.appspot.com',
+    apiKey: 'AIzaSyBp8D-xfyGmR5tQrwjuttR2BQrTTVrXq6M',
+    authDomain: 'angular13-pdv.firebaseapp.com',
+    messagingSenderId: '608200548307',
+  },
   production: false
 };
 
